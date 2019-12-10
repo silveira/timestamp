3d174ab94f52779201cd7d4d234162855f2dfa56
Silveira Neto <silveiraneto@gmail.com>
2019-12-10 12:38:41 -0500

    added a --execute option. added --suffix option.

--- timestamp.py
+++ timestamp.py
@@ -6,7 +6,6 @@ import os
 import platform
 
 timeStampFormat = '%Y_%m_%d'
-# timeStampFormat = '%Y_%m_%d_%H:%M:%S'
 
 # Given a path to a file return the creation date in a best effort
 def creation_date(path_to_file):
@@ -22,6 +21,8 @@ def creation_date(path_to_file):
 def main():
     parser = argparse.ArgumentParser(description='Rename a file to start with its timestamp')
     parser.add_argument('filename', nargs='+')
+    parser.add_argument('--execute', action='store_true')
+    parser.add_argument('--suffix', nargs='?', default='')
     args = parser.parse_args()
 
     for filename in args.filename:
@@ -30,15 +31,20 @@ def main():
        creation = creation_formated
        folder = os.path.dirname(filename)
 
-       #location = 'Iceland'
-       # newfilename = creation_formated + '_' + location + '_' + os.path.basename(filename)
+       if args.suffix == '':
+           newfilename = creation_formated + '_' + os.path.basename(filename)
+       else:
+            newfilename = creation_formated + '_' + args.suffix + '_' + os.path.basename(filename)
 
-       newfilename = creation_formated + '_' + os.path.basename(filename)
        newfile = os.path.join(folder, newfilename)
 
-       os.rename(filename, newfile)
+       if args.execute:
+         os.rename(filename, newfile)
 
        print filename, newfile
+
+    if not args.execute:
+	print 'This was a dry run. No file was modified. If you want to rename files use the --execute option.'
 
 if __name__ == "__main__":
         main()
